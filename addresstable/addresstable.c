3b0fb4120c87c6bd0132f6d24a9f6253b6109eb6
hypothe <markeagle98@gmail.com>
2021-02-05 14:49:15 +0000

    (FIX) iptab_get_next now throws errors if no (other) available nodes aer present in the network

--- addresstable/addresstable.c
+++ addresstable/addresstable.c
@@ -3,11 +3,11 @@
 // the IP address table
 iptab_addr_t __ip_table[ __IP_TABLE_LEN ] =
 {
+    { "20.67.87.126", __NOD_AVAILABLE }, 	/* Marco G.F. */
 	{ "40.68.246.18", __NOD_AVAILABLE }, 	/* Bugti */
     { "13.81.240.181", __NOD_AVAILABLE }, 	/* Federico */
     { "40.115.2.125", __NOD_AVAILABLE }, 	/* Marco S. */
     { "40.68.75.217", __NOD_AVAILABLE }, 	/* Zoe */
-    { "20.67.87.126", __NOD_AVAILABLE }, 	/* Marco G.F. */
 };
 
 int __ip_table_available_nodes = __IP_TABLE_LEN;
@@ -56,8 +56,14 @@ int iptab_get_ID_of(char* ip)
 node_id iptab_get_next( node_id my )
 {
     node_id n = my;
+	/* If there is no available node we would loop forever */
+	if (__ip_table_available_nodes < 1)	return -1;
+	
     do  n = (n+1) % __IP_TABLE_LEN;
     while ( __ip_table[n].ip_avail == __NOD_NOT_AVAILABLE );
+	/* If the calling node is the only available one in the network 
+		no communication can be established */
+	if (n == my)	return -1;
 
     return n;
 }
