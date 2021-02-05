504719d9e080f7120a76ad74893ba95c06042f5f
hypothe <markeagle98@gmail.com>
2021-02-05 15:59:28 +0000

    typo in hsh_imfir(t)st fixed

--- handshake/version.h
+++ handshake/version.h
@@ -4,6 +4,6 @@
 // the version identifier of the version is a n characters random generated string
 // the final string wll be generated with the final version
 #define __ARPNET_VER_ID_LEN 33
-#define __ARPNET_VER_ID "s861c4dh61fbvgj46bj14f6vx14scz86"
-/*	Last modified Feb-03-2021	*/
+#define __ARPNET_VER_ID "z124c6dh28pnoak71bj92i6nl33lmn34"
+/*	Last modified Feb-05-2021	*/
 #endif
