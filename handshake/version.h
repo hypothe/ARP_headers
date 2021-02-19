baf123e0c2fb0dfe420fb4890ac37a25a8450aee
hypothe <73471964+hypothe@users.noreply.github.com>
2021-02-19 09:47:13 +0100

    (UPDATE) Important changes in statistics.h

--- handshake/version.h
+++ handshake/version.h
@@ -4,6 +4,6 @@
 // the version identifier of the version is a n characters random generated string
 // the final string wll be generated with the final version
 #define __ARPNET_VER_ID_LEN 37
-#define __ARPNET_VER_ID "p48fe17h83CA5DD2hs2hw53b52o24fe72de7"
+#define __ARPNET_VER_ID "f71gn41n02FE4BA1lj7sw53q15l74kf11as9"
 /*	Last modified Feb-09-2021	*/
 #endif
