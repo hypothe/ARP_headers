f5addd9cda02f8d7817593892debbf4fdf389c02
Francesco Ganci <49157912+programmatoroSeduto@users.noreply.github.com>
2021-02-19 12:10:57 +0100

    Updated the date of the last version

--- handshake/version.h
+++ handshake/version.h
@@ -5,5 +5,5 @@
 // the final string wll be generated with the final version
 #define __ARPNET_VER_ID_LEN 37
 #define __ARPNET_VER_ID "f71gn41n02FE4BA1lj7sw53q15l74kf11as9"
-/*	Last modified Feb-09-2021	*/
+/*	Last modified Feb-19-2021	*/
 #endif
